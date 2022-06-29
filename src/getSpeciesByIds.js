b87d186e007023dae1152670c6899f915781f0f8
Pedro Luiz <pedroluizlsg@gmail.com>
2022-06-29 17:14:17 -0300

    Primeiro requisito inacabado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const speciesId = [];
+
+const compareSpecieId = (...species) => species[0].forEach((s) => {
+  const { id } = s;
+  speciesId.push(id);
+});
+
+function getSpeciesByIds(lookingForThisId) {
+  let theId = '';
+  for (let index = 0; index < speciesId.length; index += 1) {
+    if (lookingForThisId === speciesId[index]) {
+      theId = speciesId[index];
+      console.log(typeof theId);
+    }
+  }
+  console.log(data.species[0]);
 }
+compareSpecieId(data.species);
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 
 module.exports = getSpeciesByIds;
