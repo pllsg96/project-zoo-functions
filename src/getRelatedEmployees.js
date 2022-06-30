4aecee1ef7985ca699849b952aa04a0caf48a867
Pedro Luiz <pedroluizlsg@gmail.com>
2022-06-30 18:13:57 -0300

    Adiciona requisito 4 inacabado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const { employees } = data;
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (managerId.managers.length !== 0) {
+    return console.log('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
+
+function isManager(id) {
+  const allEmployees = employees.map((emp) => emp);
+  const checkIfIsManeger = allEmployees.find((employeerData) => employeerData.id === id);
+  const res = checkIfIsManeger.managers.length === 0;
+  getRelatedEmployees(checkIfIsManeger);
+  return res;
+}
+
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
