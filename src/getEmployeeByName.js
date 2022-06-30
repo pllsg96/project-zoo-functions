7af3dcb582d04e236eb5accae07b848340aff5f1
Pedro Luiz <pedroluizlsg@gmail.com>
2022-06-30 17:12:12 -0300

    Requisito 3 inacabado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+//  employees é um array de objetos
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const workerData = employees.map((employeer) => employeer);
+  const findWorker = workerData.find((worker) => worker.name === employeeName);
+  return findWorker;
 }
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
