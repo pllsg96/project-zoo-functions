7d83feb68379a985fa675d4ef96619bfe7712d5e
Pedro Luiz <pedroluizlsg@gmail.com>
2022-06-30 16:52:09 -0300

    Adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { species } = data;
+
+// species é a array de objetos
+//  return searchingTheAnimal.residents.filter((resident)=> resident.age >= age);
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const searchingTheAnimal = species.find((specie) => specie.name === animal);
+  return searchingTheAnimal.residents.every((resident)=> resident.age >= age);
+
 }
+
+console.log(getAnimalsOlderThan('giraffes',10));
 
 module.exports = getAnimalsOlderThan;
