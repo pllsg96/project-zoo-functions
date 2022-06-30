35e4d09c7c9b30ad3edf013ecf65f69913015bb1
Pedro Luiz <pedroluizlsg@gmail.com>
2022-06-30 14:37:36 -0300

    1 totalmente inacabada

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,23 +1,36 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const speciesId = [];
+const finalSpecieData = [];
 
 const compareSpecieId = (...species) => species[0].forEach((s) => {
   const { id } = s;
   speciesId.push(id);
 });
-
-function getSpeciesByIds(lookingForThisId) {
-  let theId = '';
-  for (let index = 0; index < speciesId.length; index += 1) {
-    if (lookingForThisId === speciesId[index]) {
-      theId = speciesId[index];
-      console.log(typeof theId);
-    }
-  }
-  console.log(data.species[0]);
-}
 compareSpecieId(data.species);
-getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
+
+
+
+
+
+function getSpeciesByIds(...dataSpecies) {
+  if (dataSpecies.length === 0) return [];
+  for (let i = 0; i <= dataSpecies.length; i += 1) {
+    console.log(dataSpecies[i]);
+    for (let j = 0; j < speciesId.length; j += 1) {
+      if (speciesId[j] === dataSpecies[i]) {
+        finalSpecieData.push(data.species[speciesId.indexOf(speciesId[j])]);
+      }
+    }  
+  }
+  return finalSpecieData;
+}
+getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46', '01422318-ca2d-46b8-b66c-3e9e188244ed');
 
 module.exports = getSpeciesByIds;
+
+
+// const x = speciesId.indexOf(dataSpecies.toString());
+// specieDataById.push(data.species[x]);
+// return specieDataById;
