92eef384a7a53fefffffce512d9611c12a82e159
Pedro Luiz <pedroluizlsg@gmail.com>
2022-06-30 16:54:54 -0300

    Adiciona espaço teste 1 para passar no linter

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,6 +5,4 @@ function getSpeciesByIds(...ids) {
   return ids.map((id) => species.find((specie) => specie.id === id));
 }
 
-// console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
-
 module.exports = getSpeciesByIds;
