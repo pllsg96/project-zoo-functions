d14b94d07fbc7680c31083201720b484fb7a0d4a
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-01 19:23:58 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,14 +2,18 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
-function getRelatedEmployees() {
-
-}
-
 function isManager(id) {
-  const ourWorkers = employees.some((employeer) => employeer.managers.includes(id)); // Retorna de forma genérica todos os empregados
+  const ourWorkers = employees.some((employeer) => employeer.managers.includes(id));
   return ourWorkers;
 }
+
+function getRelatedEmployees(managerId) {
+  if (isManager(managerId)) {
+    const filterResponsible = employees.filter((employ) => employ.managers.includes(managerId));
+    return filterResponsible.map((empregados) => `${empregados.firstName} ${empregados.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+}
+
 console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 // id: 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1', Nigel
