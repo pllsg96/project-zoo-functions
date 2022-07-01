4595f3c535e7f1e9cbcc167b9b18ee98c55fb51e
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-01 16:36:47 -0300

    Adiciona requisito 4 parcialmente

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,20 +3,22 @@ const data = require('../data/zoo_data');
 const { employees } = data;
 
 function getRelatedEmployees() {
-  // throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+
 }
 
 function isManager(id) {
-  const allEmployees = employees.map((emp) => emp);
-  const checkIfIsManager = allEmployees.find((employeerData) => employeerData.id === id);
-  return allEmployees.some((thereIs) => checkIfIsManager.map((managementCheck) => managementCheck.id === thereIs));
+  const ourWorkers = employees.some((employeer) => employeer.managers.includes(id)); // Retorna de forma genérica todos os empregados
+  return ourWorkers;
 }
-
 console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
-// id: 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-// firstName: 'Nigel',
+// id: 'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1', Nigel
 
-// stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+// stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992'; stephanie
 
+// const ourWokers = employees.map((employeer) => employeer); // Retorna de forma genérica todos os empregados
+//   const isThisIdAManager = ourWokers.find((worker) => worker.id === id); // Retorna o elemento com o id procurado
+//   const walkingThroughIds = ourWokers.map((worker) => worker.id); // Caminha por todos os iDs
+//   // const aaa = isThisIdAManager.responsibleFor.some(checkingEverybody());
+//   return { isThisIdAManager, walkingThroughIds };
 module.exports = { isManager, getRelatedEmployees };
