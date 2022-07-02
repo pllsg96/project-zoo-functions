2d9874add159c059eceb8f56f4e7737ab8948340
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-02 17:08:27 -0300

    Requisito 8 inacabado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,42 @@
 const data = require('../data/zoo_data');
 
+const visitors = {};
+const teste = [
+  { name: 'aaa', age: 5 },
+  { name: 'bbb', age: 50 },
+];
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const childs = entrants.filter((entrant) => entrant.age < 18);
+  const adults = entrants.filter((entrant) => (entrant.age >= 18 && entrant.age < 50));
+  const seniors = entrants.filter((entrant) => entrant.age >= 50);
+  visitors.child = (childs.length);
+  visitors.adult = (adults.length);
+  visitors.senior = (seniors.length);
+  return visitors;
+}
+
+function sumObjects(entrants) {
+  return ((entrants.child * 20.99) + (entrants.adult * 49.99) + (entrants.senior * 24.99));
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  console.log(entrants);
+  if (!entrants || (Object.values(entrants).length === 0)) return 0;
+  if (!(Array.isArray(entrants))) {
+    return sumObjects(entrants);
+  }
+  if (Array.isArray(entrants)) {
+    return entrants[0];
+  }
 }
 
+// prices: {
+//   adult: 49.99,
+//   senior: 24.99,
+//   child: 20.99,
+// },
+
+countEntrants(teste);
+console.log(calculateEntry(visitors));
 module.exports = { calculateEntry, countEntrants };
