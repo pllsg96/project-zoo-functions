15d485e07a03be2880c55dac580c0d29e45ad731
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-02 12:02:27 -0300

    Adicion

--- src/handlerElephants.js
+++ src/handlerElephants.js
@@ -33,4 +33,6 @@ const handlerElephants = (param) => {
   return computeData(param, elephants);
 };
 
+// console.log(handlerElephants('count'));
+
 module.exports = handlerElephants;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Se retorna o número 4', () => {
+    expect(handlerElephants('count').toBe(4));
+  });
+});
