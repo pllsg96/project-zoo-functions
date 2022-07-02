94191a9a356b2423c3bf044e26d2327cf9cd4ffc
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-02 10:47:53 -0300

    Requisito 5 finalizado, porém com erro de lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui 222
+const { species } = data;
+const newNamenQty = {};
+
+function checkingSex(animalSex, specieData) {
+  const { residents } = specieData;
+  if (animalSex === 'female') {
+    const residentSex = residents.filter((resident) => resident.sex === 'female');
+    return residentSex.length;
+  } if (animalSex === 'male') {
+    const residentSex = residents.filter((resident) => resident.sex === 'male');
+    return residentSex.length;
+  } return specieData.residents.length;
 }
 
+function countAnimals(animal) {
+  const speciesData = species.map((specie) => specie);
+  const speciesName = species.map((specie) => specie.name);
+  const speciesQuantity = species.map((specie) => specie.residents.length);
+  if (!animal) {
+    speciesName.map((specie, index) => newNamenQty[specie] = speciesQuantity[index]);
+    return newNamenQty;
+  }
+  const specieData = speciesData.find((specie) => specie.name === animal.specie);
+  return checkingSex(animal.sex, specieData);
+}
+console.log(countAnimals({ specie: 'penguins', sex: 'male' }));
 module.exports = countAnimals;
