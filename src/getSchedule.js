8c8f25467d82881a4d99435db5ee4b4891298784
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-03 09:56:37 -0300

    Requisito 9 inacabado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const allAnimalsAvailable = species.map((specie) => specie.availability);
+  const theAnimal = species.filter((specie) => specie.name === scheduleTarget);
+  const animalAvilable = theAnimal.map((avaiable) => avaiable.availability);
+  if (!scheduleTarget) return allAnimalsAvailable;
+  return animalAvilable[0];
 }
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
