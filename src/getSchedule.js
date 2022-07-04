f7805c93d6c10aceaa1fae31ce8b5a068e6effb5
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-04 20:17:22 -0300

    Requisito 9 inacabado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,14 +1,41 @@
-const { species } = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const outlander = {
+  Monday: {},
+  Tuesday: {},
+  Wednesday: {},
+  Thursday: {},
+  Friday: {},
+  Saturday: {},
+  Sunday: {},
+};
+
+function animalsByDay() {
+  const exist = weekDays.map((day) => species.filter((spe) => spe.availability.includes(day)));
+  const abd = exist.map((e) => e.map((y) => y.name));
+  return abd;
+}
+
+function exibAll() {
+  const a = weekDays.forEach((day) => { day.ss = '22'; });
+  return a;
+  //------------------------------------------
+  // const allInfoExibition = weekDays.reduce((map, day) => ({
+  //   ...map, [day]: `Open from ${hours[day].open}am until ${hours[day].close}`,
+  // }), {});
+  // return allInfoExibition;
+}
+
 function getSchedule(scheduleTarget) {
-  const allAnimalsAvailable = species.map((specie) => specie.availability);
   const theAnimal = species.filter((specie) => specie.name === scheduleTarget);
   const animalAvilable = theAnimal.map((avaiable) => avaiable.availability);
-  if (!scheduleTarget) return allAnimalsAvailable;
-  return animalAvilable[0];
+  if (!scheduleTarget) return exibAll();
+  return animalAvilable;
 }
 
 console.log(getSchedule());
+console.log(animalsByDay());
 
 module.exports = getSchedule;
