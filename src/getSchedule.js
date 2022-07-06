086a7c8f8cea70a6321b8133c090e23c19eee184
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-06 06:23:34 -0300

    Requisito 9 com 2 requisitos

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,41 +1,56 @@
 const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const outlander = {
-  Monday: {},
-  Tuesday: {},
-  Wednesday: {},
-  Thursday: {},
-  Friday: {},
-  Saturday: {},
-  Sunday: {},
-};
+const weekDaysOn = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const weekDaysOff = ['Monday'];
 
-function animalsByDay() {
-  const exist = weekDays.map((day) => species.filter((spe) => spe.availability.includes(day)));
-  const abd = exist.map((e) => e.map((y) => y.name));
+function animalsBySpecie(spec) {
+  const abs = species.filter((specie) => specie.name === spec);
+  const theAvaSpec = abs[0].availability;
+  return theAvaSpec;
+}
+
+function animalsByDay(day) {
+  const abd = species.filter((spe) => spe.availability.includes(day)).map((spe) => spe.name);
   return abd;
 }
 
+function turnOnDays() {
+  const allInfoExibition = weekDaysOn.reduce((map, day) => ({
+    ...map,
+    [day]: {
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+      exhibition: animalsByDay(day),
+    },
+  }), {});
+  return allInfoExibition;
+}
+
+function turnOffDays() {
+  const allInfoExibition = weekDaysOff.reduce((map, day) => ({
+    ...map,
+    [day]: {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    },
+  }), {});
+  return allInfoExibition;
+}
+
 function exibAll() {
-  const a = weekDays.forEach((day) => { day.ss = '22'; });
-  return a;
-  //------------------------------------------
-  // const allInfoExibition = weekDays.reduce((map, day) => ({
-  //   ...map, [day]: `Open from ${hours[day].open}am until ${hours[day].close}`,
-  // }), {});
-  // return allInfoExibition;
+  const a = turnOnDays();
+  const b = turnOffDays();
+  const c = { ...b, ...a };
+
+  return c;
 }
 
 function getSchedule(scheduleTarget) {
-  const theAnimal = species.filter((specie) => specie.name === scheduleTarget);
-  const animalAvilable = theAnimal.map((avaiable) => avaiable.availability);
+  if (scheduleTarget) return animalsBySpecie(scheduleTarget);
   if (!scheduleTarget) return exibAll();
-  return animalAvilable;
 }
 
-console.log(getSchedule());
-console.log(animalsByDay());
+console.log(getSchedule('elephants'));
+// animalsByDay('Tuesday');
 
 module.exports = getSchedule;
